2c70f71c34daa4df2e7dd5992454860a4a2700c3
glenrynaldyhermanus <61014001+glenrynaldyhermanus@users.noreply.github.com>
2022-07-05 20:33:28 +0700

    resend timer

--- lib/auth/auth_util.dart
+++ lib/auth/auth_util.dart
@@ -124,7 +124,7 @@ Future beginPhoneAuth({
   // * Finally modify verificationCompleted below as instructed.
   await FirebaseAuth.instance.verifyPhoneNumber(
     phoneNumber: phoneNumber,
-    timeout: Duration(seconds: 5),
+    timeout: Duration(seconds: 20),
     verificationCompleted: (phoneAuthCredential) async {
       await FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
       // If you've implemented auto-verification, navigate to home page or
